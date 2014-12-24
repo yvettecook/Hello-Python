6355f0ae80ea3c4a517913bf3f229bd132654842
Yvette <yvettecook@gmail.com>
2014-12-24 10:33:22 +0000

    More complex cave system

--- hunt_the_wumpus/game_1.py
+++ hunt_the_wumpus/game_1.py
@@ -1,9 +1,24 @@
 from random import choice
 
-cave_numbers = range(1,21)
+cave_numbers = range(0,20)
+caves = []
+
+for i in cave_numbers:
+    caves.append([])
+
+for i in cave_numbers:
+    for j in range(3):
+        passage_to = choice(cave_numbers)
+        caves[i].append(passage_to)
+
+print caves
+
 wumpus_location = choice(cave_numbers)
+wumpus_friend_location = choice(cave_numbers)
 player_location = choice(cave_numbers)
-while player_location == wumpus_location:
+
+while (player_location == wumpus_location or
+    player_location == wumpus_friend_location):
     player_location = choice(cave_numbers)
 
 print "Welcome to Hunt the Wumpus"
@@ -16,6 +31,9 @@ while True:
     if (player_location == wumpus_location - 1 or
     player_location == wumpus_location + 1):
         print "I can smell a wumpus"
+    if (player_location == wumpus_friend_location - 1 or
+    player_location == wumpus_friend_location + 1):
+        print "I can hear something noisy next door"
 
     print "What cave next?"
     player_input = raw_input(">")
@@ -27,4 +45,8 @@ while True:
         player_location = int(player_input)
         if player_location == wumpus_location:
             print "Argh, you got eaten by a wumpus!"
+            break
+
+        if player_location == wumpus_friend_location:
+            print "Argh, you got eaten by the wumpus' cunning friend!"
             break
