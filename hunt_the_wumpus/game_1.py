0ef22b37ed97f1e93cc6f7de65ecb66347033163
Yvette <yvettecook@gmail.com>
2014-12-24 11:49:21 +0000

    Added methods to Hunt the Wumpus

--- hunt_the_wumpus/game_1.py
+++ hunt_the_wumpus/game_1.py
@@ -1,3 +1,24 @@
+def create_tunnel(cave_from, cave_to):
+    caves[cave_from].append(cave_to)
+    caves[cave_to].append(cave_from)
+
+def visit_cave(cave_number):
+    visited_caves.append(cave_number)
+    unvisited_caves.remove(cave_number)
+
+
+def choose_cave(cave_list):
+  cave_number = choice(cave_list)
+  while len(caves[cave_number]) >= 3:
+    cave_number = choice(cave_list)
+  return cave_number
+
+def print_caves():
+    for number in cave_numbers:
+        print number, ":", caves[number]
+    print "---------"
+
+
 from random import choice
 
 cave_numbers = range(0,20)
@@ -6,12 +27,25 @@ caves = []
 for i in cave_numbers:
     caves.append([])
 
+unvisited_caves = range(0,20)
+visited_caves = [0]
+unvisited_caves.remove(0)
+
+while unvisited_caves != []:
+    i = choose_cave(visited_caves)
+
+    next_cave = choice(unvisited_caves)
+    create_tunnel(i, next_cave)
+    visit_cave(next_cave)
+
+    print_caves()
+
 for i in cave_numbers:
-    for j in range(3):
+    while len(caves[i]) < 3:
         passage_to = choice(cave_numbers)
         caves[i].append(passage_to)
 
-print caves
+    print_caves()
 
 wumpus_location = choice(cave_numbers)
 wumpus_friend_location = choice(cave_numbers)
@@ -28,6 +62,7 @@ print "of the cave you wish to enter next"
 
 while True:
     print "You are in cave", player_location
+    print "You can see caves", caves[player_location]
     if (player_location == wumpus_location - 1 or
     player_location == wumpus_location + 1):
         print "I can smell a wumpus"
@@ -38,8 +73,10 @@ while True:
     print "What cave next?"
     player_input = raw_input(">")
     if(not player_input.isdigit() or
-        int(player_input) not in cave_numbers):
-        print player_input, "is not a cave, doofus!"
+        int(player_input) not in caves[player_location]):
+        print "?", player_input
+        print "That's not a cave I can see (doofus)!"
+        continue
 
     else:
         player_location = int(player_input)
